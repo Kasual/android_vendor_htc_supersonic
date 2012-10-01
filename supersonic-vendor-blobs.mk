0442f8155a47cf3852f44710a7fa0a57a5bbb889
James Taylor <jmztaylor@gmail.com>
2012-10-01 16:08:42 -0400

    Revert "reinvented"
    This reverts commit 4d1996ed796a1d63654ff79b2dbacaae45293d31.

--- supersonic-vendor-blobs.mk
+++ supersonic-vendor-blobs.mk
@@ -23,37 +23,45 @@ PRODUCT_COPY_FILES += \
     vendor/htc/supersonic/proprietary/WPDB.zip:/system/etc/WPDB.zip \
     vendor/htc/supersonic/proprietary/Supersonic_20100204_Sprint_Jack_324016_v2.acdb:/system/etc/firmware/Supersonic_20100204_Sprint_Jack_324016_v2.acdb \
     vendor/htc/supersonic/proprietary/default.acdb:/system/etc/firmware/default.acdb \
+    vendor/htc/supersonic/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
+    vendor/htc/supersonic/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
+    vendor/htc/supersonic/proprietary/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
+    vendor/htc/supersonic/proprietary/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/htc/supersonic/proprietary/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
+    vendor/htc/supersonic/proprietary/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
+    vendor/htc/supersonic/proprietary/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
+    vendor/htc/supersonic/proprietary/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
     vendor/htc/supersonic/proprietary/libOmxVdec.so:/system/lib/libOmxVdec.so \
     vendor/htc/supersonic/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
     vendor/htc/supersonic/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \
     vendor/htc/supersonic/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \
-    vendor/htc/supersonic/proprietary/wimaxDumpKmsg:/system/bin/wimaxDumpKmsg \
+    vendor/htc/supersonic/proprietary/libgsl.so:/system/lib/libgsl.so \
     vendor/htc/supersonic/proprietary/apph:/system/bin/apph \
-    vendor/htc/supersonic/proprietary/wimaxDumpLogcat:/system/bin/wimaxDumpLogcat \
-    vendor/htc/supersonic/proprietary/setWiMAXPropDaemond:/system/bin/setWiMAXPropDaemond \
-    vendor/htc/supersonic/proprietary/wimax_mtd:/system/bin/wimax_mtd \
-    vendor/htc/supersonic/proprietary/sequansd:/system/bin/sequansd \
-    vendor/htc/supersonic/proprietary/getWiMAXPropDaemond:/system/bin/getWiMAXPropDaemond \
-    vendor/htc/supersonic/proprietary/wimaxDumpLastKmsg:/system/bin/wimaxDumpLastKmsg \
-    vendor/htc/supersonic/proprietary/wimaxDhcpRenew:/system/bin/wimaxDhcpRenew \
-    vendor/htc/supersonic/proprietary/wimaxDhcpRelease:/system/bin/wimaxDhcpRelease \
-    vendor/htc/supersonic/proprietary/wimaxAddRoute:/system/bin/wimaxAddRoute \
-    vendor/htc/supersonic/proprietary/DefaultTree.xml:/system/etc/wimax/sequansd/DefaultTree.xml \
-    vendor/htc/supersonic/proprietary/wimaxDhcp.conf:/system/etc/wimax/dhcp/wimaxDhcp.conf \
     vendor/htc/supersonic/proprietary/ipd:/system/bin/ipd \
-    vendor/htc/supersonic/proprietary/ip:/system/bin/ip \
-    vendor/htc/supersonic/proprietary/sequansd_app.xml:/system/etc/wimax/sequansd/sequansd_app.xml \
-    vendor/htc/supersonic/proprietary/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    vendor/htc/supersonic/proprietary/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    vendor/htc/supersonic/proprietary/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/htc/supersonic/proprietary/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/supersonic/proprietary/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/supersonic/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/supersonic/proprietary/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-    vendor/htc/supersonic/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/supersonic/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
-    vendor/htc/supersonic/proprietary/libOmxVidEnc.so:system/lib/libOmxVidEnc.so
-    vendor/htc/supersonic/proprietary/libqc-opt.so:system/lib/libqc-opt.so \
-    vendor/htc/supersonic/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-    vendor/htc/supersonic/proprietary/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-    vendor/htc/supersonic/proprietary/libwvm.so:system/vendor/lib/libwvm.so
+    vendor/htc/supersonic/proprietary/libqc-opt.so:/system/lib/libqc-opt.so \
+    vendor/htc/supersonic/proprietary/gps.supersonic.so:/system/lib/hw/gps.supersonic.so \
+    vendor/htc/supersonic/proprietary/lights.supersonic.so:/system/lib/hw/lights.supersonic.so \
+    vendor/htc/supersonic/proprietary/sensors.supersonic.so:/system/lib/hw/sensors.supersonic.so \
+    vendor/htc/supersonic/proprietary/libdivxdrmdecrypt.so:/system/lib/libdivxdrmdecrypt.so \
+    vendor/htc/supersonic/proprietary/libC2D2.so:/system/lib/libC2D2.so \
+    vendor/htc/supersonic/proprietary/libcneapiclient.so:/system/lib/libneapiclient.so \
+    vendor/htc/supersonic/proprietary/libcneqmiutils.so:/system/lib/libcneqmiutils.so \
+    vendor/htc/supersonic/proprietary/libdiag.so:/system/lib/libdiag.so \
+    vendor/htc/supersonic/proprietary/libdnshostprio.so:/system/lib/libdnshostprio.so \
+    vendor/htc/supersonic/proprietary/libmm-color-convertor.so:/system/lib/libmm-color-convertor.so \
+    vendor/htc/supersonic/proprietary/libmmosal.so:/system/lib/libmmosal.so \
+    vendor/htc/supersonic/proprietary/libmmparser.so:/system/lib/libmmparser.so \
+    vendor/htc/supersonic/proprietary/libmmparser_divxdrmlib.so:/system/lib/libmmparser_divxdrmlib.so \
+    vendor/htc/supersonic/proprietary/libnetmonitor.so:/system/lib/libnetmonitor.so \
+    vendor/htc/supersonic/proprietary/libOpenVG.so:/system/lib/libOpenVG.so \
+    vendor/htc/supersonic/proprietary/libtcpfinaggr.so:/system/lib/libtcfinaggr.so \
+    vendor/htc/supersonic/proprietary/libv8.so:/system/lib/libv8.so \
+    vendor/htc/supersonic/proprietary/pp_proc_plugin.so:/system/lib/pp_proc_plugin.so \
+    vendor/htc/supersonic/proprietary/qnet-plugin.so:/system/lib/qnet-plugin.so \
+    vendor/htc/supersonic/proprietary/tcp-connections.so:/system/lib/tcp-connections.so \
+    vendor/htc/supersonic/proprietary/dmagent:/system/bin/dmagent \
+    vendor/htc/supersonic/proprietary/netmgrd:/system/bin/netmgrd \
+    vendor/htc/supersonic/proprietary/qmuxd:/system/bin/qmuxd \
+    vendor/htc/supersonic/proprietary/libnetmgr.so:/system/lib/libnetmgr.so \
+    vendor/htc/supersonic/proprietary/audio.primary.qsd8k.so:/system/lib/hw/audio.primary.qsd8k.so \
+    vendor/htc/supersonic/proprietary/audio_policy.qsd8k.so:/system/lib/hw/audio_policy.qsd8k.so
